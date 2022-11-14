665aedbee62049bc8e120162f83f7a4fad45d748
Oviedo F <oviedofederico039@gmail.com>
2022-11-14 09:12:00 -0300

    user.model

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,6 +1,5 @@
 const { Schema, model } = require("mongoose");
 const bcrypt = require('bcryptjs');
-
 const userSchema = new Schema({
     userImage: String,
     name: {
@@ -71,7 +70,6 @@ userSchema.statics.encryptPassword = async (password) => {
     const salt = await bcrypt.genSalt(10);
     return await bcrypt.hash(password, salt);
 }
-
 userSchema.statics.comparePassword = async (password, receivedPassword) => {
     return await bcrypt.compare(password, receivedPassword);
 }
